8139082950f6eb901b0cde5c28fa5159eece458a
Christine Samarchi <christinesamarchi@christines-air.lan>
2018-09-30 14:41:53 -0500

    made a deck object and gave it a shuffle method

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
-//Declaring Variables
-let deck = new Array();
-let suits = ['spades', 'hearts', 'clubs', 'diamonds'];
-let values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'King', 'Queen'];
+//
+//
+// JackBlack Game
+// Created by Christine Samarchi
+//
+//
 
-//Create The Deck
-const getDeck = () => {
+//Deck Class
+class Deck {
+  //Creates a Deck Object
+  //This constructor provides default values for the suits and values properties and an empty array value for cards
+  constructor() {
+    this.suits = ['spades', 'hearts', 'clubs', 'diamonds'];
+    this.values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+    this.cards = []
+  }
 
-  let deck = new Array();
-
-  for (let i = 0; i < suits.length; i++) {
-
-    for (let x = 0; x < values.length; x++) {
-
-      let card = {Value: values[i], Suit: suits[x]};
-
-      deck.push(card)
+  //Method to Loop through and create all 52 cards for a deck and store them in the "cards" property
+  createDeck() {
+    for (let i = 0; i < this.suits.length; i++) {
+      for (let x = 0; x < this.values.length; x++) {
+        this.cards.push(new Card(this.values[x], this.suits[i]))
+      }
     }
   }
-  return deck;
+
+  //Method to shuffle the cards
+  shuffleCards() {
+    for (let i = 0; i < 1000; i++) {
+      let location1 = Math.floor((Math.random() * this.cards.length));
+      let location2 = Math.floor((Math.random() * this.cards.length));
+      let tmp = this.cards[location1];
+
+      this.cards[location1] = this.cards[location2];
+      this.cards[location2] = tmp;
+    }
+  }
+  
 }
 
-console.log(getDeck());
+//Card Class
+class Card {
+  //Creates a Card Object
+  //Constructor that requires a value and suit to create a Card Object
+  constructor(value, suit) {
+    this.value = value
+    this.suit = suit
+  }
+}
+
+//Call new deck and create the cards
+let deck = new Deck()
+deck.createDeck()
+deck.shuffleCards()
+
+//Print cards to console for sanity check
+console.log(deck.cards)
